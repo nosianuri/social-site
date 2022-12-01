c5854297f091137967d279bd2bb77f05c96beaf5
nosianuri <nurinosia@gmail.com>
2022-12-02 02:00:20 +0600

    basic setup

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
 import logo from './logo.svg';
 import './App.css';
+// import Navbar from './components/navbar/Navbar';
+// import Header from './components/header/Header';
+// import Feature from './components/features/Feature';
+// import Download from './components/download/Download';
+// import Subscribe from './components/subscribe/Subscribe';
+// import Faq from './components/faq/Faq';
+// import Footer from './components/footer/Footer';
+
+import {Navbar, Header, Feature, Download, Subscribe, Faq, Footer} from "./components";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <main>
+      <header className='header-bg'>
+        <Navbar />
+        <Header />
       </header>
-    </div>
+      <Feature />
+      <Download />
+      <Subscribe />
+      <Faq />
+      <Footer />
+    </main>
   );
 }
 
