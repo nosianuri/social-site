40fc820d9894eb4389fe75b1e4c318a87c6a9543
nosianuri <nurinosia@gmail.com>
2023-01-04 00:38:25 +0600

    testing run

--- src/pages/Shared/Navbar.js
+++ src/pages/Shared/Navbar.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Link, NavLink } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 import logo from "../../assets/Untitled-design-6-300x100.png";
 import './Navbar.css';
 
